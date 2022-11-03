4f88eb59208e4580a82a9b6284f90ab0d9eaa941
StellaZen <109876174+StellaZen@users.noreply.github.com>
2022-11-03 16:15:11 -0300

    tentativas js

--- interactive-rating/app.js
+++ interactive-rating/app.js
@@ -2,7 +2,7 @@ var submit = document.getElementById("submit");
 var conteiner = document.getElementById("conteiner");
 var conteiner2 = document.getElementById("conteiner2");
 
-submit.addEventListener("click", () => ;{
+submit.addEventListener("click", () => {
     conteiner.style.display = "none";
     conteiner2.style.display = "block";
 })
