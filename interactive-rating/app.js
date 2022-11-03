3738ff71f8cec0bff8ec785af574584a9d2f7967
StellaZen <109876174+StellaZen@users.noreply.github.com>
2022-11-03 12:59:27 -0300

    testando js

--- interactive-rating/app.js
+++ interactive-rating/app.js
@@ -1,3 +1,9 @@
-function changeColor(){
-    document.getElementById("score1").style.backgroundColor = document.getElementById("gray").value;
-}
+var submit = document.getElementById("submit");
+var conteiner = document.getElementById("conteiner");
+var conteiner2 = document.getElementById("conteiner2");
+
+submit.addEventListener("click", () => ;{
+    conteiner.style.display = "none";
+    conteiner2.style.display = "block";
+})
+
