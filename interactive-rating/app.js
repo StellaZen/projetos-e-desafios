647193d50102f60271042654c87ea024347f0fe6
StellaZen <109876174+StellaZen@users.noreply.github.com>
2022-11-03 21:46:21 -0300

    teste :/

--- interactive-rating/app.js
+++ interactive-rating/app.js
@@ -1,11 +1,22 @@
-var submit = document.getElementById("submit");
-var conteiner = document.getElementById("conteiner");
-var conteiner2 = document.getElementById("conteiner2");
+var rating = 0;
 
-submit.addEventListener("click", trocar());
-
-function trocar(){
-    conteiner.style.display = "none";
-    conteiner2.style.display = "block";
+function rate(userRating){
+    document.querySelector(".submit").style.cursor = "pointer";
+    rating = userRating + 1;
+    document.querySelector(`.r${userRating + 1}`).classList.add("rating_active");
+    for(var i = 0; i < 5; i++){
+        if(i != userRating){
+            document.querySelector(`.r${i + 1}`).classList.remove("rating_active");
+        }
+    }
 }
+
+document.querySelector(".rating-form").addEventListener("submit", function(e){
+    e.preventDefault();
+    if(rating !=0){
+        document.querySelector(".conteiner").style.display = "none";
+        document.querySelector(".rating-selection").innerText = "You selected " + rating + " out of 5";
+        document.querySelector(".conteiner2").style.display = "flex";
+    }
+})
 
