2f0189ea2120b913df325c9c585760f7becef1e8
StellaZen <109876174+StellaZen@users.noreply.github.com>
2022-11-06 08:32:53 -0300

    + testes js

--- interactive-rating/app.js
+++ interactive-rating/app.js
@@ -1,22 +1,7 @@
-var rating = 0;
+var cor = document.querySelectorAll(".rating-button");
+addEventListener ("click",cor)
 
-function rate(userRating){
-    document.querySelector(".submit").style.cursor = "pointer";
-    rating = userRating + 1;
-    document.querySelector(`.r${userRating + 1}`).classList.add("rating_active");
-    for(var i = 0; i < 5; i++){
-        if(i != userRating){
-            document.querySelector(`.r${i + 1}`).classList.remove("rating_active");
-        }
-    }
+function mudaCor(){
+    cor.style.background = ("gray");
 }
-
-document.querySelector(".rating-form").addEventListener("submit", function(e){
-    e.preventDefault();
-    if(rating !=0){
-        document.querySelector(".conteiner").style.display = "none";
-        document.querySelector(".rating-selection").innerText = "You selected " + rating + " out of 5";
-        document.querySelector(".conteiner2").style.display = "flex";
-    }
-})
 
