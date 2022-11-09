7859051aae9560d75000cc0e3f00780a776e1b0d
StellaZen <109876174+StellaZen@users.noreply.github.com>
2022-11-09 08:37:25 -0300

    Update app.js

--- interactive-rating/app.js
+++ interactive-rating/app.js
@@ -2,11 +2,10 @@ var botao = document.getElementById("submit")
 botao.addEventListener("click",trocar)
 
 
-var conteiner1 = document.getElementById("conteiner")
-var conteiner2 = document.getElementById("conteiner2")
-
 function trocar(){
-    conteiner1.style.display = "none"
-    conteiner2.style.display = "block"
+    var conteiner1 = document.getElementById("conteiner");
+    var conteiner2 = document.getElementById("conteiner2");
+        conteiner1.style.display = "none";
+        conteiner2.style.display = "block";
 }
 
