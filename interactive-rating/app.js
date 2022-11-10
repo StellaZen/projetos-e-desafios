8b992672afa3f1d076e4e0f6e8abe939d7e21974
StellaZen <109876174+StellaZen@users.noreply.github.com>
2022-11-10 12:40:39 -0300

    cores

--- interactive-rating/app.js
+++ interactive-rating/app.js
@@ -27,28 +27,28 @@ function numero(){
         label5.style.backgroundColor = "hsl(213, 19%, 21%)";
         numeroSelecionado = '1';   
     }else if(num[1].checked){
-        label2.style.backgroundColor = "lightgray";
+        label2.style.backgroundColor = "hsl(216, 12%, 54%)";
         label1.style.backgroundColor = "hsl(213, 19%, 21%)";
         label3.style.backgroundColor = "hsl(213, 19%, 21%)";
         label4.style.backgroundColor = "hsl(213, 19%, 21%)";
         label5.style.backgroundColor = "hsl(213, 19%, 21%)";
         numeroSelecionado = '2';
     }else if(num[2].checked){
-        label3.style.backgroundColor = "lightgray";
+        label3.style.backgroundColor = "hsl(216, 12%, 54%)";
         label1.style.backgroundColor = "hsl(213, 19%, 21%)";
         label2.style.backgroundColor = "hsl(213, 19%, 21%)";
         label4.style.backgroundColor = "hsl(213, 19%, 21%)";
         label5.style.backgroundColor = "hsl(213, 19%, 21%)";
         numeroSelecionado = '3';
     }else if(num[3].checked){
-        label4.style.backgroundColor = "lightgray";
+        label4.style.backgroundColor = "hsl(216, 12%, 54%)";
         label1.style.backgroundColor = "hsl(213, 19%, 21%)";
         label2.style.backgroundColor = "hsl(213, 19%, 21%)";
         label3.style.backgroundColor = "hsl(213, 19%, 21%)";
         label5.style.backgroundColor = "hsl(213, 19%, 21%)";
         numeroSelecionado = '4';
     }else{
-        label5.style.backgroundColor = "lightgray";
+        label5.style.backgroundColor = "hsl(216, 12%, 54%)";
         label1.style.backgroundColor = "hsl(213, 19%, 21%)";
         label2.style.backgroundColor = "hsl(213, 19%, 21%)";
         label3.style.backgroundColor = "hsl(213, 19%, 21%)";
