f6f324ca5779660eff6dd0a49e5f28fafc78e15f
StellaZen <109876174+StellaZen@users.noreply.github.com>
2022-11-18 10:39:40 -0300

    n sei

--- article-preview-component/app.js
+++ article-preview-component/app.js
@@ -1,11 +1,13 @@
 var botao = document.getElementById("botao");
 var nome = document.getElementById("nome")
 var icons = document.getElementById("icons");
+
 botao.addEventListener("click",muda);
 function muda(e){
     e.preventDefault();
     nome.style.display = "none";
     icons.style.display = "flex";
 }
+
 
 
